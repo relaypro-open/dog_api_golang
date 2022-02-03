a4536d096d717c8e413de531e9fc0644858ddc2e
Drew Gulino <dgulino@relaypro.com>
2022-02-03 16:08:13 -0500

    pretty printing of structs

--- main/main.go
+++ main/main.go
@@ -9,12 +9,12 @@ func main() {
 	c := api.NewClient("my-key")
 
 	HostsRes, HostsErr := c.GetHosts(nil)
-	fmt.Printf("res: %v\n", HostsRes)
-	fmt.Printf("err: %v\n", HostsErr)
+	fmt.Printf("Hosts: %+v\n", HostsRes)
+	fmt.Printf("Error: %+v\n", HostsErr)
 
 	HostId := "eda000f6-0743-448f-874b-a7703ecddfaa"
-	res, err := c.GetHost(HostId, nil)
-	fmt.Printf("res: %v\n", res)
-	fmt.Printf("err: %v\n", err)
+	HostRes, HostErr := c.GetHost(HostId, nil)
+	fmt.Printf("Host: %+v\n", HostRes)
+	fmt.Printf("Error: %+v\n", HostErr)
 
 }
