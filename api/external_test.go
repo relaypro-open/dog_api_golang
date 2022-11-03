f5c29ac689c94f81d64ce46fb4676582bd5a01af
Drew Gulino <dgulino@relaypro.com>
2022-11-03 18:11:35 -0400

    disabled non-functional external tests

---
+++ api/external_test.go
@@ -0,0 +1,41 @@
+//go:build integration || external
+
+package api
+
+//import (
+//	"os"
+//	"testing"
+//
+//	"github.com/stretchr/testify/assert"
+//)
+
+//func TestExternalIntegration(t *testing.T) {
+//	DoTestGetExternals(t)                                     //R
+//}
+//
+//func DoTestGetExternals(t *testing.T) {
+//	c := NewClient(os.Getenv("DOG_API_KEY"), os.Getenv("DOG_API_ENDPOINT"))
+//
+//	res, statusCode, err := c.GetExternals(nil)
+//	assert.Equal(t, 200, statusCode)
+//	assert.Nil(t, err, "expecting nil error")
+//	assert.NotNil(t, res, "expecting non-nil result")
+//	t.Logf("res[0]: %+v\n", res[0])
+//
+//	assert.NotEmpty(t, res[0].ID, "expecting non-empty ID")
+//}
+//
+//func DoTestGetExternal(t *testing.T, ExternalID string) (External External) {
+//	c := NewClient(os.Getenv("DOG_API_KEY"), os.Getenv("DOG_API_ENDPOINT"))
+//
+//	res, statusCode, err := c.GetExternal(ExternalID, nil)
+//
+//	assert.Equal(t, 200, statusCode)
+//	assert.Nil(t, err, "expecting nil error")
+//	assert.NotNil(t, res, "expecting non-nil result")
+//	t.Logf("res: %+v\n", res)
+//
+//	assert.NotEmpty(t, res.ID, "expecting non-empty ID")
+//	assert.Equal(t, res.ID, ExternalID)
+//	return res
+//}
