c91e80ff8ce9c78ebf8430a627eb1e9fced00905
dgulino <1379399+dgulinobw@users.noreply.github.com>
2024-02-09 11:17:29 -0500

    embed git hash in USER-AGENT

--- api/dog.go
+++ api/dog.go
@@ -1,8 +1,14 @@
 package api
+	
 
 import (
 	"github.com/go-resty/resty/v2"
+    _ "embed"
 )
+
+//go:generate sh -c "printf %s $(git rev-parse HEAD) > VERSION.txt"
+//go:embed VERSION.txt
+var Commit string
 
 type Client struct {
 	*resty.Client
@@ -12,12 +18,12 @@ func NewClient(apiToken string, apiEndpoint string) *Client {
 	client := Client{}
 	client.Client = resty.New()
 	client.SetAuthToken(apiToken)
-	client.SetHostURL(apiEndpoint)
+	client.SetBaseURL(apiEndpoint)
 	// Headers for all request
 	client.SetHeader("Accept", "application/json")
 	client.SetHeaders(map[string]string{
 		"Content-Type": "application/json",
-		"User-Agent":   "dog_go_api",
+		"User-Agent":   "dog_go_api_" + Commit,
 	})
 	return &client
 }
