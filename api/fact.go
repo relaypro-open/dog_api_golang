b4fa516a6d562dac09045d91e68891e8c0e3b1a2
Drew Gulino <1379399+dgulinobw@users.noreply.github.com>
2024-04-30 12:00:49 -0400

    trying new terraform 'any' type constraint

--- api/fact.go
+++ api/fact.go
@@ -1,15 +1,10 @@
 package api
 
 import (
-//	"encoding/json"
-//	"errors"
 	"strconv"
-//	"log"
 )
 
 type FactsList []Fact
-
-type FactsListJson []FactJson
 
 type FactsListOptions struct {
 	Limit int `json:"limit"`
@@ -22,19 +17,7 @@ type Fact struct {
 	Groups map[string]*FactGroup `json:"groups"`
 }
 
-type FactJson struct {
-	ID     string                    `json:"id,omitempty"`
-	Name   string                    `json:"name"`
-	Groups map[string]*FactGroupJson `json:"groups"`
-}
-
 type FactGroup struct {
-	Vars     any                          `json:"vars,omitempty"`
-	Hosts    map[string]map[string]any    `json:"hosts"`
-	Children []string                     `json:"children"`
-}
-
-type FactGroupJson struct {
 	Vars     map[string]any               `json:"vars,omitempty"`
 	Hosts    map[string]map[string]any    `json:"hosts"`
 	Children []string                     `json:"children"`
@@ -60,231 +43,7 @@ type FactCreateResponse struct {
 	Result string `json:"result"`
 }
 
-//func encodeFact(factJson FactJson) (fact Fact) {
-//	factEncoded := Fact{}
-//	encodedGroups := map[string]*FactGroup{}
-//	encodedHosts := make(map[string]map[string]*string)
-//	for name, group := range factJson.Groups {
-//		if group.Hosts != nil && group.Vars != nil {
-//			for name, host := range group.Hosts {
-//				for key, value := range host {
-//					groupHostsVar, _ := json.Marshal(value)
-//					groupHostsVarsString := string(groupHostsVar)
-//					encodedHosts[name] = make(map[string]*string)
-//					encodedHosts[name][key] = &groupHostsVarsString
-//				}
-//			}
-//			responseVars, _ := json.Marshal(group.Vars)
-//			varsString := string(responseVars)
-//			encodedGroup := FactGroup{
-//				Vars:     &varsString,
-//				Hosts:    encodedHosts,
-//				Children: group.Children,
-//			}
-//			encodedGroups[name] = &encodedGroup
-//		} else if group.Hosts == nil && group.Vars != nil {
-//			responseVars, _ := json.Marshal(group.Vars)
-//			varsString := string(responseVars)
-//			encodedGroup := FactGroup{
-//				Vars:     &varsString,
-//				Children: group.Children,
-//			}
-//			encodedGroups[name] = &encodedGroup
-//		} else if group.Hosts != nil && group.Vars == nil {
-//			for name, host := range group.Hosts {
-//				for key, value := range host {
-//					groupHostsVar, _ := json.Marshal(value)
-//					groupHostsVarsString := string(groupHostsVar)
-//					encodedHosts[name] = make(map[string]*string)
-//					encodedHosts[name][key] = &groupHostsVarsString
-//				}
-//			}
-//			encodedGroup := FactGroup{
-//				Hosts:    encodedHosts,
-//				Children: group.Children,
-//			}
-//			encodedGroups[name] = &encodedGroup
-//		} else if group.Hosts == nil && group.Vars == nil {
-//			encodedGroup := FactGroup{
-//				Children: group.Children,
-//			}
-//			encodedGroups[name] = &encodedGroup
-//		}
-//	}
-//	factEncoded.Groups = encodedGroups
-//	factEncoded.Name = factJson.Name
-//	factEncoded.ID = factJson.ID
-//	return factEncoded
-//}
-//
-//func DecodeFact(fact Fact) (factJson FactJson, unmarshalErr error) {
-//	newGroups := map[string]*FactGroupJson{}
-//	for name, group := range fact.Groups {
-//		newGroup := FactGroupJson{}
-//		if group.Vars != nil && group.Hosts != nil {
-//			var vars = map[string]any{}
-//			_ = json.Unmarshal([]byte(*group.Vars), &vars)
-//			newGroup.Vars = vars
-//			decodedHosts := map[string]map[string]any{}
-//			for name, host := range group.Hosts {
-//				for key, value := range host {
-//					var vars = map[string]any{}
-//					unmarshalErr = json.Unmarshal([]byte(*value), &vars)
-//					log.Printf("unmarshalErr: %+v\n", unmarshalErr)
-//					decodedHosts[name] = make(map[string]any)
-//					decodedHosts[name][key] = vars
-//				}
-//			}
-//			unmarshalErr = json.Unmarshal([]byte(*group.Vars), &decodedHosts)
-//			newGroup.Hosts = decodedHosts
-//		} else if group.Vars == nil && group.Hosts != nil {
-//			decodedHosts := make(map[string]map[string]any)
-//			log.Printf("decodedHosts: %+v\n", decodedHosts)
-//			for name, host := range group.Hosts {
-//				for key, value := range host {
-//					//log.Printf("value: %+v\n", value)
-//					log.Printf("name, %v, key: %v\n", name, key)
-//					vars := map[string]any{}
-//					_ = json.Unmarshal([]byte(*value), &vars)
-//					//log.Printf("vars: %+v\n", vars)
-//					decodedHosts[name] = make(map[string]any)
-//					decodedHosts[name][key] = vars
-//				}
-//			}
-//			newGroup.Hosts = decodedHosts
-//		} else if group.Vars != nil && group.Hosts == nil {
-//			var vars = map[string]any{}
-//			_ = json.Unmarshal([]byte(*group.Vars), &vars)
-//			newGroup.Vars = vars
-//		}
-//		newGroup.Children = group.Children
-//		newGroups[name] = &newGroup
-//	}
-//	factDecoded := FactJson{}
-//	factDecoded.Groups = newGroups
-//	factDecoded.Name = fact.Name
-//	factDecoded.ID = fact.ID
-//	return factDecoded, unmarshalErr
-//}
-
-//func (c *Client) GetFactsEncode(options *FactsListOptions) (factsList FactsList, statusCode int, Error error) {
-//	limit := 100
-//	page := 1
-//	if options != nil {
-//		limit = options.Limit
-//		page = options.Page
-//	}
-//
-//	resp, err := c.Client.R().
-//	SetResult(&FactsListJson{}).
-//	SetQueryParams(map[string]string{
-//		"page_no": strconv.Itoa(page),
-//		"limit":   strconv.Itoa(limit),
-//	}).
-//	Get("/facts")
-//
-//	result := (*resp.Result().(*FactsListJson))
-//	encodeFacts := FactsList{}
-//	for _, factJson := range result {
-//		encodeFacts = append(encodeFacts, encodeFact(factJson))
-//	}
-//	return encodeFacts, resp.StatusCode(), err
-//}
-//
-//func (c *Client) GetFactEncode(FactID string, options *FactListOptions) (fact Fact, statusCode int, Error error) {
-//	limit := 100
-//	page := 1
-//	if options != nil {
-//		limit = options.Limit
-//		page = options.Page
-//	}
-//
-//	resp, err := c.Client.R().
-//	SetResult(&FactJson{}).
-//	SetQueryParams(map[string]string{
-//		"page_no": strconv.Itoa(page),
-//		"limit":   strconv.Itoa(limit),
-//	}).
-//	SetPathParams(map[string]string{
-//		"FactID": FactID,
-//	}).
-//	Get("/fact/{FactID}")
-//
-//	result := (*resp.Result().(*FactJson))
-//	factEncoded := encodeFact(result)
-//	return factEncoded, resp.StatusCode(), err
-//
-//}
-//
-//func (c *Client) GetFactByNameEncode(FactName string, options *FactListOptions) (fact Fact, statusCode int, Error error) {
-//	limit := 100
-//	page := 1
-//	if options != nil {
-//		limit = options.Limit
-//		page = options.Page
-//	}
-//
-//	resp, err := c.Client.R().
-//	SetResult(&FactJson{}).
-//	SetQueryParams(map[string]string{
-//		"page_no": strconv.Itoa(page),
-//		"limit":   strconv.Itoa(limit),
-//	}).
-//	SetPathParams(map[string]string{
-//		"FactName": FactName,
-//	}).
-//	Get("/fact?name={FactName}")
-//
-//	result := (*resp.Result().(*FactJson))
-//	factEncoded := encodeFact(result)
-//	return factEncoded, resp.StatusCode(), err
-//
-//}
-//
-//func (c *Client) UpdateFactEncode(FactID string, factUpdate Fact, options *FactListOptions) (fact Fact, statusCode int, Error error) {
-//	//file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-//	//if err != nil {
-//	//	log.Fatal(err)
-//	//}
-//
-//	//log.SetOutput(file)
-//
-//	factDecoded, responseVarsErr := DecodeFact(factUpdate)
-//	resp, respErr := c.Client.R().
-//	SetResult(&FactJson{}).
-//	SetPathParams(map[string]string{
-//		"FactID": FactID,
-//	}).
-//	SetBody(factDecoded).
-//	Put("/fact/{FactID}")
-//
-//	result := (*resp.Result().(*FactJson))
-//	factEncoded := encodeFact(result)
-//
-//	err := errors.Join(respErr, responseVarsErr)
-//	return factEncoded, resp.StatusCode(), err
-//}
-//
-//func (c *Client) CreateFactEncode(factNew Fact, options *FactListOptions) (fact Fact, statusCode int, Error error) {
-//
-//	//PrettyPrint("factNew", factNew)
-//	factDecoded, responseVarsErr := DecodeFact(factNew)
-//	//PrettyPrint("factDecoded", factDecoded)
-//	resp, respErr := c.Client.R().
-//	SetResult(&FactJson{}).
-//	SetBody(factDecoded).
-//	Post("/fact")
-//
-//	result := (*resp.Result().(*FactJson))
-//	//PrettyPrint("fact result", result)
-//	factEncoded := encodeFact(result)
-//	//PrettyPrint("factEncoded", factEncoded)
-//	err := errors.Join(respErr, responseVarsErr)
-//
-//	return factEncoded, resp.StatusCode(), err
-//}
-
-func (c *Client) GetFacts(options *FactsListOptions) (factsList FactsListJson, statusCode int, Error error) {
+func (c *Client) GetFacts(options *FactsListOptions) (factsList FactsList, statusCode int, Error error) {
 	limit := 100
 	page := 1
 	if options != nil {
@@ -293,18 +52,18 @@ func (c *Client) GetFacts(options *FactsListOptions) (factsList FactsListJson, s
 	}
 
 	resp, err := c.Client.R().
-	SetResult(&FactsListJson{}).
+	SetResult(&FactsList{}).
 	SetQueryParams(map[string]string{
 		"page_no": strconv.Itoa(page),
 		"limit":   strconv.Itoa(limit),
 	}).
 	Get("/facts")
 
-	result := (*resp.Result().(*FactsListJson))
+	result := (*resp.Result().(*FactsList))
 	return result, resp.StatusCode(), err
 }
 
-func (c *Client) GetFact(FactID string, options *FactListOptions) (fact FactJson, statusCode int, Error error) {
+func (c *Client) GetFact(FactID string, options *FactListOptions) (fact Fact, statusCode int, Error error) {
 	limit := 100
 	page := 1
 	if options != nil {
@@ -313,7 +72,7 @@ func (c *Client) GetFact(FactID string, options *FactListOptions) (fact FactJson
 	}
 
 	resp, err := c.Client.R().
-	SetResult(&FactJson{}).
+	SetResult(&Fact{}).
 	SetQueryParams(map[string]string{
 		"page_no": strconv.Itoa(page),
 		"limit":   strconv.Itoa(limit),
@@ -323,12 +82,12 @@ func (c *Client) GetFact(FactID string, options *FactListOptions) (fact FactJson
 	}).
 	Get("/fact/{FactID}")
 
-	result := (*resp.Result().(*FactJson))
+	result := (*resp.Result().(*Fact))
 	return result, resp.StatusCode(), err
 
 }
 
-func (c *Client) GetFactByName(FactName string, options *FactListOptions) (fact FactJson, statusCode int, Error error) {
+func (c *Client) GetFactByName(FactName string, options *FactListOptions) (fact Fact, statusCode int, Error error) {
 	limit := 100
 	page := 1
 	if options != nil {
@@ -337,7 +96,7 @@ func (c *Client) GetFactByName(FactName string, options *FactListOptions) (fact 
 	}
 
 	resp, err := c.Client.R().
-	SetResult(&FactJson{}).
+	SetResult(&Fact{}).
 	SetQueryParams(map[string]string{
 		"page_no": strconv.Itoa(page),
 		"limit":   strconv.Itoa(limit),
@@ -347,33 +106,33 @@ func (c *Client) GetFactByName(FactName string, options *FactListOptions) (fact 
 	}).
 	Get("/fact?name={FactName}")
 
-	result := (*resp.Result().(*FactJson))
+	result := (*resp.Result().(*Fact))
 	return result, resp.StatusCode(), err
 
 }
 
-func (c *Client) UpdateFact(FactID string, FactUpdate FactJson, options *FactListOptions) (fact FactJson, statusCode int, Error error) {
+func (c *Client) UpdateFact(FactID string, FactUpdate Fact, options *FactListOptions) (fact Fact, statusCode int, Error error) {
 
 	resp, err := c.Client.R().
-	SetResult(&FactJson{}).
+	SetResult(&Fact{}).
 	SetPathParams(map[string]string{
 		"FactID": FactID,
 	}).
 	SetBody(FactUpdate).
 	Put("/fact/{FactID}")
 
-	result := (*resp.Result().(*FactJson))
+	result := (*resp.Result().(*Fact))
 	return result, resp.StatusCode(), err
 }
 
-func (c *Client) CreateFact(factNew FactJson, options *FactListOptions) (fact FactJson, statusCode int, Error error) {
+func (c *Client) CreateFact(factNew Fact, options *FactListOptions) (fact Fact, statusCode int, Error error) {
 
 	resp, err := c.Client.R().
-	SetResult(&FactJson{}).
+	SetResult(&Fact{}).
 	SetBody(factNew).
 	Post("/fact")
 
-	result := (*resp.Result().(*FactJson))
+	result := (*resp.Result().(*Fact))
 	return result, resp.StatusCode(), err
 }
 
--- api/fact_test.go
+++ api/fact_test.go
@@ -18,157 +18,7 @@ func TestFactIntegration(t *testing.T) {
 	updatedFact := DoTestGetFact(t, FactCreateResponse.ID)
 	assert.Equal(t, "name_update", updatedFact.Name)
 	DoTestDeleteFact(t, FactCreateResponse.ID) //D
-
-	//FactCreateResponseEncode := DoTestCreateFactEncode(t) //C
-	//t.Logf("Id: %v", FactCreateResponseEncode.ID)
-	//DoTestGetFactsEncode(t)                                     //R
-	//DoTestGetFactEncode(t, FactCreateResponseEncode.ID)         //R
-	//DoTestGetFactByNameEncode(t, FactCreateResponseEncode.Name) //R
-	//DoTestUpdateFactEncode(t, FactCreateResponseEncode.ID)      //U
-	//updatedFactEncode := DoTestGetFactEncode(t, FactCreateResponseEncode.ID)
-	//assert.Equal(t, "name_update", updatedFactEncode.Name)
-	//DoTestDeleteFact(t, FactCreateResponseEncode.ID) //D
 }
-
-// *Encode functions are for use by Terraform
-
-//func DoTestGetFactsEncode(t *testing.T) {
-//	c := NewClient(os.Getenv("DOG_API_TOKEN"), os.Getenv("DOG_API_ENDPOINT"))
-//
-//	res, statusCode, err := c.GetFactsEncode(nil)
-//	assert.Equal(t, 200, statusCode)
-//	t.Logf("err: %+v\n", err)
-//	assert.Nil(t, err, "expecting nil error")
-//	assert.NotNil(t, res, "expecting non-nil result")
-//	t.Logf("res[0].ID %s\n", res[0].ID)
-//
-//	assert.NotEmpty(t, res[0].ID, "expecting non-empty Rules")
-//}
-//
-//func DoTestGetFactEncode(t *testing.T, FactID string) (Fact Fact) {
-//	c := NewClient(os.Getenv("DOG_API_TOKEN"), os.Getenv("DOG_API_ENDPOINT"))
-//
-//	res, statusCode, err := c.GetFactEncode(FactID, nil)
-//
-//	assert.Equal(t, 200, statusCode)
-//	assert.Nil(t, err, "expecting nil error")
-//	assert.NotNil(t, res, "expecting non-nil result")
-//	t.Logf("res: %+v\n", res)
-//
-//	assert.NotEmpty(t, res.ID, "expecting non-empty ID")
-//	assert.Equal(t, res.ID, FactID)
-//	return res
-//}
-//
-//func DoTestGetFactByNameEncode(t *testing.T, FactName string) (Fact Fact) {
-//	c := NewClient(os.Getenv("DOG_API_TOKEN"), os.Getenv("DOG_API_ENDPOINT"))
-//
-//	res, statusCode, err := c.GetFactByNameEncode(FactName, nil)
-//
-//	assert.Equal(t, 200, statusCode)
-//	assert.Nil(t, err, "expecting nil error")
-//	assert.NotNil(t, res, "expecting non-nil result")
-//	t.Logf("res: %+v\n", res)
-//
-//	assert.NotEmpty(t, res.ID, "expecting non-empty ID")
-//	assert.Equal(t, res.Name, FactName)
-//	return res
-//}
-//
-//func DoTestUpdateFactEncode(t *testing.T, FactID string) (fact Fact) {
-//	c := NewClient(os.Getenv("DOG_API_TOKEN"), os.Getenv("DOG_API_ENDPOINT"))
-//
-//	Vars1 := map[string]any{
-//	    "environment": "mob_dev",
-//	    "dog_env": "dev",
-//	    "boolean": true,
-//	    "integer": 1,
-//        }
-//
-//	//S1 := "Linux"
-//	//S2 := "sql"
-//
-//	//Hosts1 := map[string]map[string]*string{
-//	//	"web.test.abc": map[string]*string{"os": &S1},
-//	//	"db.test.abc":  map[string]*string{"db": &S2},
-//	//}
-//	Hosts1 := map[string]map[string]any{
-//		"web.test.abc": map[string]any{"os": "Linux"},
-//		"db.test.abc":  map[string]any{"db": "sql"},
-//	}
-//
-//	Children1 := []string{"test"}
-//
-//	Ig1 := &FactGroupJson{
-//		Vars:     Vars1,
-//		Hosts:    Hosts1,
-//		Children: Children1}
-//
-//	update := FactJson{
-//		Name:   "name_update",
-//		Groups: map[string]*FactGroupJson{"mob_dev": Ig1},
-//	}
-//
-//	updateEncoded := encodeFact(update)
-//
-//	res, statusCode, err := c.UpdateFactEncode(FactID, updateEncoded, nil)
-//
-//	assert.Nil(t, err, "expecting nil error")
-//	assert.NotNil(t, res, "expecting non-nil result")
-//	t.Logf("res: %+v\n", res)
-//
-//	assert.NotEmpty(t, res.ID, "expecting non-empty ID")
-//	assert.Equal(t, "name_update", res.Name)
-//	assert.Equal(t, 200, statusCode)
-//	return res
-//}
-//
-//func DoTestCreateFactEncode(t *testing.T) (fact Fact) {
-//	c := NewClient(os.Getenv("DOG_API_TOKEN"), os.Getenv("DOG_API_ENDPOINT"))
-//
-//	//Vars1 := `{
-//	//    "environment": "mob_dev",
-//	//    "dog_env": "dev",
-//	//    "boolean": true,
-//	//    "integer": 1
-//	//    }`
-//	//M1 := *string{"os": "Linux"}
-//	//M2 := *string{"db": "sql"}
-//
-//	//Hosts1 := map[string]map[string]*string{
-//	//	"web.test.abc": map[string]&M1,
-//	//	"db.test.abc":  map[string]&M2,
-//	//}
-//	
-//	Hosts1 := map[string]map[string]any{
-//		"web.test.abc": map[string]any{"os": "Linux"},
-//		"db.test.abc":  map[string]any{"db": "sql"},
-//	}
-//
-//	Children1 := []string{"test"}
-//
-//	Ig1 := &FactGroupJson{
-//		Hosts:    Hosts1,
-//		Children: Children1}
-//
-//	newFact := FactJson{
-//		Name:   "name",
-//		Groups: map[string]*FactGroupJson{"mob_dev": Ig1},
-//	}
-//
-//	newFactEncoded := encodeFact(newFact)
-//
-//	res, statusCode, err := c.CreateFactEncode(newFactEncoded, nil)
-//	assert.Equal(t, 201, statusCode)
-//	assert.NotEmpty(t, res.ID, "expected non-empty ID")
-//	assert.Nil(t, err, "expecting nil error")
-//	assert.NotNil(t, res, "expecting non-nil result")
-//	t.Logf("err: %v", err)
-//	t.Logf("res: %+v\n", res)
-//	return res
-//}
-
-//Non-Terraform functions
 
 func DoTestGetFacts(t *testing.T) {
 	c := NewClient(os.Getenv("DOG_API_TOKEN"), os.Getenv("DOG_API_ENDPOINT"))
@@ -183,7 +33,7 @@ func DoTestGetFacts(t *testing.T) {
 	assert.NotEmpty(t, res[0].ID, "expecting non-empty Rules")
 }
 
-func DoTestGetFact(t *testing.T, FactID string) (Fact FactJson) {
+func DoTestGetFact(t *testing.T, FactID string) (Fact Fact) {
 	c := NewClient(os.Getenv("DOG_API_TOKEN"), os.Getenv("DOG_API_ENDPOINT"))
 
 	res, statusCode, err := c.GetFact(FactID, nil)
@@ -198,7 +48,7 @@ func DoTestGetFact(t *testing.T, FactID string) (Fact FactJson) {
 	return res
 }
 
-func DoTestGetFactByName(t *testing.T, FactName string) (Fact FactJson) {
+func DoTestGetFactByName(t *testing.T, FactName string) (Fact Fact) {
 	c := NewClient(os.Getenv("DOG_API_TOKEN"), os.Getenv("DOG_API_ENDPOINT"))
 
 	res, statusCode, err := c.GetFactByName(FactName, nil)
@@ -213,7 +63,7 @@ func DoTestGetFactByName(t *testing.T, FactName string) (Fact FactJson) {
 	return res
 }
 
-func DoTestUpdateFact(t *testing.T, FactID string) (Fact FactJson) {
+func DoTestUpdateFact(t *testing.T, FactID string) (Fact Fact) {
 	c := NewClient(os.Getenv("DOG_API_TOKEN"), os.Getenv("DOG_API_ENDPOINT"))
 
 	Vars1 := map[string]any{
@@ -230,11 +80,11 @@ func DoTestUpdateFact(t *testing.T, FactID string) (Fact FactJson) {
 
 	Children1 := []string{"test"}
 
-	Ig1 := &FactGroupJson{Vars1, Hosts1, Children1}
+	Ig1 := &FactGroup{Vars1, Hosts1, Children1}
 
-	update := FactJson{
+	update := Fact{
 		Name:   "name_update",
-		Groups: map[string]*FactGroupJson{"mob_dev": Ig1},
+		Groups: map[string]*FactGroup{"mob_dev": Ig1},
 	}
 
 	res, statusCode, err := c.UpdateFact(FactID, update, nil)
@@ -249,7 +99,7 @@ func DoTestUpdateFact(t *testing.T, FactID string) (Fact FactJson) {
 	return res
 }
 
-func DoTestCreateFact(t *testing.T) (fact FactJson) {
+func DoTestCreateFact(t *testing.T) (fact Fact) {
 	c := NewClient(os.Getenv("DOG_API_TOKEN"), os.Getenv("DOG_API_ENDPOINT"))
 
 	Vars1 := map[string]any{
@@ -266,11 +116,11 @@ func DoTestCreateFact(t *testing.T) (fact FactJson) {
 
 	Children1 := []string{"test"}
 
-	Ig1 := &FactGroupJson{Vars1, Hosts1, Children1}
+	Ig1 := &FactGroup{Vars1, Hosts1, Children1}
 
-	newFact := FactJson{
+	newFact := Fact{
 		Name:   "name",
-		Groups: map[string]*FactGroupJson{"mob_dev": Ig1},
+		Groups: map[string]*FactGroup{"mob_dev": Ig1},
 	}
 
 	res, statusCode, err := c.CreateFact(newFact, nil)
