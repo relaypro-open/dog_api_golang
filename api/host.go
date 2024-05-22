19b58fb8055dfba7c8c2821873d7fc9496be65b5
Drew Gulino <1379399+dgulinobw@users.noreply.github.com>
2024-05-22 17:04:33 -0400

    alert_enable no longer omitempty

--- api/host.go
+++ api/host.go
@@ -14,7 +14,7 @@ type Host struct {
 	Location    string `json:"location"`
 	Name        string `json:"name"`
 	Vars        string `json:"vars"` //raw json for Terraform
-	AlertEnable bool  `json:"alert_enable,omitempty"`
+	AlertEnable bool   `json:"alert_enable"`
 }
 
 type HostJson struct {
@@ -25,7 +25,7 @@ type HostJson struct {
 	Location    string         `json:"location,omitempty"`
 	Name        string         `json:"name,omitempty"`
 	Vars        map[string]any `json:"vars,omitempty"` //parsed json
-	AlertEnable bool          `json:"alert_enable,omitempty"`
+	AlertEnable bool           `json:"alert_enable,omitempty"`
 }
 
 type HostListOptions struct {
@@ -42,7 +42,7 @@ type HostUpdateRequest struct {
 	Location    string `json:"location,omitempty"`
 	Name        string `json:"name,omitempty"`
 	Vars        string `json:"vars,omitempty"`
-	AlertEnable bool  `json:"alert_enable,omitempty"`
+	AlertEnable bool   `json:"alert_enable,omitempty"`
 }
 
 type HostCreateRequest struct {
@@ -52,7 +52,7 @@ type HostCreateRequest struct {
 	Location    string `json:"location"`
 	Name        string `json:"name"`
 	Vars        string `json:"vars"`
-	AlertEnable bool  `json:"alert_enable,omitempty"`
+	AlertEnable bool   `json:"alert_enable"`
 }
 
 type HostCreateResponse struct {
@@ -63,7 +63,7 @@ type HostCreateResponse struct {
 	Location    string `json:"location"`
 	Name        string `json:"name"`
 	Vars        string `json:"vars"`
-	AlertEnable bool  `json:"alert_enable,omitempty"`
+	AlertEnable bool   `json:"alert_enable"`
 }
 
 type HostsList []Host
@@ -267,7 +267,7 @@ func (c *Client) CreateHostEncode(hostNew Host, options *HostListOptions) (host 
 	PrettyPrint("result", result)
 	hostEncoded, responseVarsErr := encodeHost(result)
 	PrettyPrint("hostEncoded", hostEncoded)
-	err := errors.Join(respErr, decodeErr,responseVarsErr)
+	err := errors.Join(respErr, decodeErr, responseVarsErr)
 	return hostEncoded, resp.StatusCode(), err
 }
 
